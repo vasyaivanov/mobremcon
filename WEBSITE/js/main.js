d4a6134dd2956855fd296fe6dc5df5f090e2f958
vasyaivanov <wizard_files3@yahoo.com>
2014-10-08 15:16:55 -0700

    Added a panel for video chat. Changed some web css

--- WEBSITE/js/main.js
+++ WEBSITE/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 		$('#uploadPresentation').change(function(){
-				$('body').append('<div class="overlay"><div class="upload-text">Uploading your presentation. You will get a unique url to share.</div><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div></div>');
+				$('body').append('<div class="overlay"><div class="upload-text">Uploading...<br>YOU WILL GET A UNIQUE URL TO SHARE.</div><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div></div>');
 				setTimeout(function() {
 							window.location = "http://slite.us/A1";
 					}, 3000);
