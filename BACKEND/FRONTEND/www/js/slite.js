2ad50c3ad5bdc79a23f3f616671701435185fb87
Mikhail Arov <mike.arov@gmail.com>
2014-10-17 09:05:41 -0700

    Fixes laser pointer line moving and adds videochat panel to A2
    Also removes disabling arrow keys

--- BACKEND/FRONTEND/www/js/slite.js
+++ BACKEND/FRONTEND/www/js/slite.js
@@ -56,7 +56,7 @@ function thumbnails() {
 function changeURL() {
 	document.getElementById('theIframe').src = "http://slite.us/" + document.getElementById("URLSlides").value;
 	if (document.getElementById("URLSlides").value == "A1") socket = io.connect('http://slite.elasticbeanstalk.com:1337');
-	else socket = io.connect('http://slite-dev.elasticbeanstalk.com:1337');
+	else socket = io.connect('http://slite.elasticbeanstalk.com:1337');
 	document.getElementById('theIframe').src += '';
 	currSlideNum = 0;
 	$("#notes").text(notesArray[currSlideNum]);
