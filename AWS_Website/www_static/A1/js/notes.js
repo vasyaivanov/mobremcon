df07be753f0fd904ced52df7b4c8ecd343fefd20
kraskoshnyy <kraskoshnyy@gmail.com>
2015-04-14 13:38:34 -0700

    Notes added

---
+++ AWS_Website/www_static/A1/js/notes.js
@@ -0,0 +1,39 @@
+ $( document ).ready(function() {
+
+var noteSaveTimeout = 0;
+
+// Initialize
+$.ajax({
+	type: "POST",
+	dataType: "json",
+	url: "../notes.html",
+	data: {slideId: document.location.pathname, initialize: 1},
+	success: function(data){
+		if(data.code == 2) {
+			if(data.note) {
+				$(".paper").html(data.note);
+			}
+		}
+	 }
+ });
+
+
+$( ".paper" ).keypress(function() {
+	clearTimeout(noteSaveTimeout);
+	noteSaveTimeout = setTimeout(saveNoteData,3000);
+
+});
+
+var saveNoteData = function () {
+	$.ajax({
+		type: "POST",
+		dataType: "json",
+		url: "../notes.html",
+		data: {slideId: document.location.pathname, noteText: $( ".paper" ).html()},
+		success: function(data){
+			// Future functions. Possible data.code - 3 - saved, 10 - slide was not found
+		 }
+ 	});
+}
+
+});
