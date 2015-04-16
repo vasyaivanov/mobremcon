2c203f7bcf3e7f98411b6b472d90d13c7e78c1e7
vasyaivanov <wizard_files3@yahoo.com>
2015-04-15 22:16:04 -0700

    Styling for Remote Control

--- AWS_Website/www_static/A1/js/A1.js
+++ AWS_Website/www_static/A1/js/A1.js
@@ -160,7 +160,7 @@ function showRemote() {
         pathName = pathName.replace('/', '');
     }
     var newUrl = "http://" + document.location.hostname + "/remote?presentation=" + pathName;
-    window.open(newUrl, "popupWindow", "width=1050,height=700, scrollbars=no"); //,scrollbars=no
+    window.open(newUrl, "popupWindow", "width=1250,height=730, scrollbars=no"); //,scrollbars=no
 	showHideMenu(true);
 }
 
