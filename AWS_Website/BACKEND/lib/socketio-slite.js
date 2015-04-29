c5c4c0899b5944855bf1ee22583195ac61042fbf
Mikhail Arov <mike.arov@gmail.com>
2015-04-29 00:35:15 -0700

    Chat integrated into our infrastructure.
    Both on BE and FE advanced-chat
    (http://tamas.io/chat-2-0-supercharged-chat-written-in-node-js-and-socke
    t-io/) is now integrated with slite. One room per slite hash.
    TODO: User login integration.

--- AWS_Website/BACKEND/lib/socketio-slite.js
+++ AWS_Website/BACKEND/lib/socketio-slite.js
@@ -4,6 +4,9 @@ var url = require('url')
   , path = require('path')
   , prepare_slite = require('./prepare_slite.js')
   , converter = require('./converter.js')
+  , _ = require('underscore')._
+  , Room = require('./room.js')
+  , uuid = require('node-uuid')
   , SLITE_EXT = '.jpg'
   , SLIDE_REG_EXP = new RegExp('^img\\d+' + SLITE_EXT + '$')
   , HTML5_UPLOADER = false;
@@ -49,7 +52,49 @@ function pollUpdate () {
 var pollStatisticsArray = new Array();
 var pollAnswerArray = new Array();
 
-var clients = [];
+var people = {};
+var rooms = {};
+var sockets = [];
+var chatHistory = {};
+
+function purge(s, action) {
+    if (people[s.id].inroom) { //user is in a room
+    var room = rooms[people[s.id].inroom]; //check which room user is in.
+        if (action === "disconnect") {
+            module.parent.exports.io.sockets.emit("update", people[s.id].name + " has disconnected from the server.");
+            if (_.contains((room.people), s.id)) {
+                var personIndex = room.people.indexOf(s.id);
+                room.people.splice(personIndex, 1);
+                s.leave(room.name);
+            }
+            delete people[s.id];
+            sizePeople = _.size(people);
+            module.parent.exports.io.sockets.emit("update-people", {people: people, count: sizePeople});
+            var o = _.findWhere(sockets, {'id': s.id});
+            sockets = _.without(sockets, o);
+        } else if (action === "removeRoom") {
+            s.emit("update", "Only the owner can remove a room.");
+        } else if (action === "leaveRoom") {
+            if (_.contains((room.people), s.id)) {
+                var personIndex = room.people.indexOf(s.id);
+                room.people.splice(personIndex, 1);
+                people[s.id].inroom = null;
+                module.parent.exports.io.sockets.emit("update", people[s.id].name + " has left the room.");
+                s.leave(room.name);
+            }
+        }   
+    } else {
+        //The user isn't in a room, but maybe he just disconnected, handle the scenario:
+        if (action === "disconnect") {
+            module.parent.exports.io.sockets.emit("update", people[s.id].name + " has disconnected from the server.");
+            delete people[s.id];
+            sizePeople = _.size(people);
+            module.parent.exports.io.sockets.emit("update-people", {people: people, count: sizePeople});
+            var o = _.findWhere(sockets, {'id': s.id});
+            sockets = _.without(sockets, o);
+        }       
+    }
+}
 
 console.log('in remote control');
 
@@ -208,24 +253,176 @@ module.parent.exports.io.sockets.on('connection', function (socket) {
     });
 
 
-/*
-	for(var i=0;i<5;i++){
+    socket.on("joinserver", function(name, device) {
+        var exists = false;
+        var ownerRoomID = inRoomID = null;
 
-			console.log('test=' + i);
-			sleep.sleep(5);
+        _.find(people, function(key,value) {
+            if (key.name.toLowerCase() === name.toLowerCase())
+                return exists = true;
+        });
+        if (exists) {//provide unique username:
+            var randomNumber=Math.floor(Math.random()*1001)
+            do {
+                proposedName = name+randomNumber;
+                _.find(people, function(key,value) {
+                    if (key.name.toLowerCase() === proposedName.toLowerCase())
+                        return exists = true;
+                });
+            } while (!exists);
+            socket.emit("exists", {msg: "The username already exists, please pick another one.", proposedName: proposedName});
+        } else {
+            people[socket.id] = {"name" : name, "owns" : ownerRoomID, "inroom": inRoomID, "device": device};
+            socket.emit("update", "You have connected to the server.");
+            module.parent.exports.io.sockets.emit("update", people[socket.id].name + " is online.")
+            sizePeople = _.size(people);
+            sizeRooms = _.size(rooms);
+            module.parent.exports.io.sockets.emit("update-people", {people: people, count: sizePeople});
+            socket.emit("roomList", {rooms: rooms, count: sizeRooms});
+            socket.emit("joined"); //extra emit for GeoLocation
+            sockets.push(socket);
+        }
+    });
 
-			db.get('name', function (err, value) {
-				if (err) return console.log('Ooops!', err) // likely the key was not found
-				// ta da!
-				console.log('name2=' + value)
-			})
+    socket.on("getOnlinePeople", function(fn) {
+                fn({people: people});
+        });
 
+    socket.on("countryUpdate", function(data) { //we know which country the user is from
+        country = data.country.toLowerCase();
+        people[socket.id].country = country;
+        module.parent.exports.io.sockets.emit("update-people", {people: people, count: sizePeople});
+    });
 
+    socket.on("typing", function(data) {
+        if (typeof people[socket.id] !== "undefined")
+            module.parent.exports.io.sockets.in(socket.room).emit("isTyping", {isTyping: data, person: people[socket.id].name});
+    });
+    
+    socket.on("send", function(msg) {
+        //process.exit(1);
+        var re = /^[w]:.*:/;
+        var whisper = re.test(msg);
+        var whisperStr = msg.split(":");
+        var found = false;
+        if (whisper) {
+            var whisperTo = whisperStr[1];
+            var keys = Object.keys(people);
+            if (keys.length != 0) {
+                for (var i = 0; i<keys.length; i++) {
+                    if (people[keys[i]].name === whisperTo) {
+                        var whisperId = keys[i];
+                        found = true;
+                        if (socket.id === whisperId) { //can't whisper to ourselves
+                            socket.emit("update", "You can't whisper to yourself.");
+                        }
+                        break;
+                    } 
+                }
+            }
+            if (found && socket.id !== whisperId) {
+                var whisperTo = whisperStr[1];
+                var whisperMsg = whisperStr[2];
+                socket.emit("whisper", {name: "You"}, whisperMsg);
+                module.parent.exports.io.sockets.socket(whisperId).emit("whisper", people[socket.id], whisperMsg);
+            } else {
+                socket.emit("update", "Can't find " + whisperTo);
+            }
+        } else {
+            module.parent.exports.io.sockets.in(socket.room).emit("chat", people[socket.id], msg);
+            socket.emit("isTyping", false);
+            if (_.size(chatHistory[socket.room]) > 10) {
+                chatHistory[socket.room].splice(0,1);
+            } else {
+                chatHistory[socket.room].push(people[socket.id].name + ": " + msg);
+            }
+        }
+    });
 
-			socket.emit('news', { hello: i });
-			socket.on('my other event', function (data) {
-				console.log(data);
-			});
-	  } */
-    //});
+    socket.on("disconnect", function() {
+        if (typeof people[socket.id] !== "undefined") { //this handles the refresh of the name screen
+            purge(socket, "disconnect");
+        }
+    });
+
+    //Room functions
+    socket.on("createRoom", function(name) {
+        if (people[socket.id].inroom) {
+            //socket.emit("update", "You are in a room. Please leave it first to create your own.");
+        } else if (!people[socket.id].owns) {
+            var id = uuid.v4();
+            var room = new Room(name, id, socket.id);
+            rooms[id] = room;
+            sizeRooms = _.size(rooms);
+            module.parent.exports.io.sockets.emit("roomList", {rooms: rooms, count: sizeRooms});
+            //add room to socket, and auto join the creator of the room
+            socket.room = name;
+            socket.join(socket.room);
+            people[socket.id].owns = id;
+            people[socket.id].inroom = id;
+            room.addPerson(socket.id);
+            //socket.emit("update", "Welcome to " + room.name + ".");
+            socket.emit("sendRoomID", {id: id});
+            chatHistory[socket.room] = [];
+        } else {
+            socket.emit("update", "You have already created a room.");
+        }
+    });
+
+    socket.on("check", function(name, fn) {
+        var match = false;
+        _.find(rooms, function(key,value) {
+            if (key.name === name)
+                return match = true;
+        });
+        fn({result: match});
+    });
+
+    socket.on("removeRoom", function(id) {
+         var room = rooms[id];
+         if (socket.id === room.owner) {
+            purge(socket, "removeRoom");
+        } else {
+                    socket.emit("update", "Only the owner can remove a room.");
+        }
+    });
+
+    socket.on("joinRoom", function(roomName) {
+        if (typeof people[socket.id] !== "undefined") {
+            var room = _.find(rooms, function(room) {return (room.name === roomName)} );
+            var id = room.id;
+            if (socket.id === room.owner) {
+                socket.emit("update", "You are the owner of this room and you have already been joined.");
+            } else {
+                if (_.contains((room.people), socket.id)) {
+                    socket.emit("update", "You have already joined this room.");
+                } else {
+                    if (people[socket.id].inroom !== null) {
+                            //socket.emit("update", "You are already in a room ("+rooms[people[socket.id].inroom].name+"), please leave it first to join another room.");
+                        } else {
+                        room.addPerson(socket.id);
+                        people[socket.id].inroom = id;
+                        socket.room = room.name;
+                        socket.join(socket.room);
+                        user = people[socket.id];
+                        module.parent.exports.io.sockets.in(socket.room).emit("update", user.name + " has connected to " + room.name + " room.");
+                        //socket.emit("update", "Welcome to " + room.name + ".");
+                        socket.emit("sendRoomID", {id: id});
+                        var keys = _.keys(chatHistory);
+                        if (_.contains(keys, socket.room)) {
+                            socket.emit("history", chatHistory[socket.room]);
+                        }
+                    }
+                }
+            }
+        } else {
+            socket.emit("update", "Please enter a valid name first.");
+        }
+    });
+
+    socket.on("leaveRoom", function(id) {
+        var room = rooms[id];
+        if (room)
+            purge(socket, "leaveRoom");
+    });
 }); // module.parent.exports.io.sockets.on('connection' ...
