e9b949c7fc507a79c06c32b0a8cf41e3fa8b4ff9
vladislav-i <saintmz3@gmail.com>
2015-10-31 03:10:51 -0700

    [refactor] Added sing up section to index2

--- AWS_Website/www_static/new_template/js/customJs.js
+++ AWS_Website/www_static/new_template/js/customJs.js
@@ -1,16 +1,26 @@
 $(function() {
     $("#login2").hide();
+    $("#signUp").hide();
 
     //hide all data and show login
     $( ".loginUser" ).click(function() {
         $(".allData").hide();
         $("#login2").show();
+        $("#signUp").hide();
     });
 
     //hide login and show all data
     $( ".menuLink" ).click(function() {
         $(".allData").show();
-        $("#{}login2").hide();
+        $("#login2").hide();
+        $("#signUp").hide();
+    });
+
+    //show sign up hide everything else
+    $( ".signUpUser" ).click(function() {
+        $(".allData").hide();
+        $("#login2").hide();
+        $("#signUp").show();
     });
     
 });
