8db2eda661ebd44fc24efcc8bae3c4a6ef9fd3eb
root <kraskoshnyy@gmail.com>
2016-03-19 15:16:28 -0700

    Fixes for pres & facebook

--- AWS_Website/BACKEND/lib/routes.js
+++ AWS_Website/BACKEND/lib/routes.js
@@ -2,7 +2,7 @@ module.exports = function(settings) {
 
   // Facebook return & auth
   settings.router.get('/facebook/return',
-    settings.passport.authenticate('facebook', { failureRedirect: '/#login' }),
+    settings.passport.authenticate('facebook', { failureRedirect: '/?facebook-err=1#login' }),
     function(req, res) {
       res.redirect('/#top');
     }
