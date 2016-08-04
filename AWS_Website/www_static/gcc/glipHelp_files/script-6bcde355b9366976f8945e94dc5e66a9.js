3d79ab30f94175ea0f2fad622f8b25c346434142
vasyaivanov <wizard_files3@yahoo.com>
2016-08-04 09:02:35 -0700

    Added RingCentral demo (glipHelp) to gcc folder
    Glip - is a product of RingCentral

---
+++ AWS_Website/www_static/gcc/glipHelp_files/script-6bcde355b9366976f8945e94dc5e66a9.js
@@ -0,0 +1,34 @@
+/*
+ * jQuery v1.9.1 included
+ */
+
+$(document).ready(function() {
+
+  // social share popups
+  $(".share a").click(function(e) {
+    e.preventDefault();
+    window.open(this.href, "", "height = 500, width = 500");
+  });
+
+  // toggle the share dropdown in communities
+  $(".share-label").on("click", function(e) {
+    e.stopPropagation();
+    var isSelected = this.getAttribute("aria-selected") == "true";
+    this.setAttribute("aria-selected", !isSelected);
+    $(".share-label").not(this).attr("aria-selected", "false");
+  });
+
+  $(document).on("click", function() {
+    $(".share-label").attr("aria-selected", "false");
+  });
+
+  // show form controls when the textarea receives focus
+  $(".answer-body textarea").one("focus", function() {
+    $(".answer-form-controls").show();
+  });
+
+  $(".comment-container textarea").one("focus", function() {
+    $(".comment-form-controls").show();
+  });
+
+});
